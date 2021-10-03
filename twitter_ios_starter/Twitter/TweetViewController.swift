4b49f270a7fccbd6922bceb1681b6a942e5c48f3
ash193 <52557802+ash193@users.noreply.github.com>
2021-10-02 22:01:15 -0400

    Updates

---
+++ twitter_ios_starter/Twitter/TweetViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Ashley R on 10/2/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func tweet(_ sender: Any) {
+    }
+ 
+}
